8253c1e48badf7446bfaab3236c9e54efde0e1a5
parsonsmatt <parsonsmatt@gmail.com>
2019-04-01 13:53:32 -0600

    Add Fields

--- src/RecordWrangler.hs
+++ src/RecordWrangler.hs
@@ -1,6 +1,15 @@
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs               #-}
+{-# LANGUAGE QuasiQuotes         #-}
+{-# LANGUAGE RankNTypes          #-}
+{-# LANGUAGE RecordWildCards     #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell     #-}
+{-# LANGUAGE TypeApplications    #-}
 
 -- | This module contains a Template Haskell helper to produce a new datatype
 -- with modified field names. The initial use case is to allow for easier record
@@ -50,21 +59,26 @@ module RecordWrangler
     , fieldLabelModifier
     , constructorModifier
     , typeNameModifier
+    , addFields
+    , field
+    , NewField
+    , Proxy(..)
     ) where
 
+import GHC.TypeLits
 import           Data.Traversable
+import           Data.Typeable
 import           Language.Haskell.TH
-import           Language.Haskell.TH.Quote
 
 -- | The options for wrangling records. The constructor is hidden so that
 -- we can add new features and powers without breaking your code!
 data WrangleOpts = WrangleOpts
-    { fieldLabelModifier :: String -> String
+    { fieldLabelModifier  :: String -> String
     -- ^ This function will be applied to every field label in the provided
     -- record.
     --
     -- @since 0.1.0.0
-    , typeNameModifier :: String -> String
+    , typeNameModifier    :: String -> String
     -- ^ This function will be applied to the type name.
     --
     -- @since 0.1.0.0
@@ -72,7 +86,76 @@ data WrangleOpts = WrangleOpts
     -- ^ This function will be applied to the constructor name.
     --
     -- @since 0.1.0.0
+    , addFields           :: [NewField]
+    -- ^ Add the following fields to the datatype. These will be inserted
+    -- afterwards, and will have *exactly* the name you provide - the
+    -- 'fieldLabelModifier' function *will not* be applied to this value.
     }
+
+-- | A new field to add to the datatype. Use the function 'field' to create
+-- values of this type.
+--
+-- @since 0.1.1.0
+data NewField where
+   NewField :: String -> Bang -> Q Type -> NewField
+
+-- | Add a new field to the given record. For simple types, you can simply
+-- pass in the name:
+--
+-- @
+-- field "userName" ''String
+-- @
+--
+-- If the type is more complicated than a single name, then you can use the
+-- type quasiquoter, like so:
+--
+-- @
+-- field "userName" [t|Char -> Maybe String|]
+--
+-- @since 0.1.1.0
+field :: IsType typ => String -> typ -> NewField
+field name typ = NewField name (Bang NoSourceUnpackedness NoSourceStrictness) (toType typ)
+
+class IsType typ where
+    toType :: typ -> Q Type
+
+instance IsType (Q Type) where
+    toType = id
+
+instance IsType Name where
+    toType nm = do
+        info <- reify nm
+        case info of
+            TyConI dec -> case dec of
+                DataD _cxt name _tyVars _mkind _cons _derivs ->
+                    pure (ConT name)
+                NewtypeD _cxt name _tyVars _mkind _con _derivs ->
+                    pure (ConT name)
+                TySynD name _tyVars _typ ->
+                    pure (ConT name)
+                _ -> fail
+                    $ "Expected a data, newtype, or type synonym. You gave me: "
+                    <> show dec
+
+            PrimTyConI name _arity _unlifted ->
+                pure (ConT name)
+
+            FamilyI _dec _instances ->
+                fail
+                    $ "I don't know how to handle FamilyI yet."
+            _ ->
+                fail
+                    $ "Expected a name referring to a Type, but you gave me "
+                    <> "'" <> show nm <> "' which refers to this: "
+                    <> show info
+
+instance {-# OVERLAPPABLE #-}
+    ( TypeError
+        ( 'Text "The argument to 'field' must either be a QuasiQuoted type or a Name referrinng to a type. You gave me an: "
+        ':$$: 'ShowType x)
+    ) => IsType x
+  where
+    toType = undefined
 
 -- | This is the default set of 'WrangleOpts'. It affixes a @'@ character to
 -- the end of the fields, type, and constructor. If you want different behavior,
@@ -88,6 +171,7 @@ defWrangleOpts = WrangleOpts
     { fieldLabelModifier = (++ "'")
     , typeNameModifier = (++ "'")
     , constructorModifier = (++ "'")
+    , addFields = []
     }
 
 -- | Create a new datatype with altered field labels, type name, and constructor
@@ -139,11 +223,17 @@ wrangle tyName WrangleOpts {..} = do
                 $ "Expected a record constructor, but got: "
                 <> show constr
 
+    newFields <-
+        for addFields $ \(NewField fieldName bang' qtyp) -> do
+            typ <- qtyp
+            pure (mkName fieldName, bang', typ)
+
     let newConstrs = flip map recConstrs $ \(recName, fields) ->
           ( modifyName constructorModifier recName
-          , flip map fields $ \(fieldName, bang', typ) ->
+          , (++ newFields) . flip map fields $ \(fieldName, bang', typ) ->
               (modifyName fieldLabelModifier fieldName, bang', typ)
           )
+        newTypes = map (\(_, _, t) -> t) newFields
 
     let mkPatternFrom (recName, _) vars =
             ConP recName $ map VarP vars
@@ -151,6 +241,7 @@ wrangle tyName WrangleOpts {..} = do
             for fields $ \_ -> newName "x"
         mkBodyFrom (recName, _) vars =
             NormalB $ foldl AppE (ConE recName) (map VarE vars)
+
 
     convClauses <-
         for (zip recConstrs newConstrs) $ \(constr, newConstr) -> do
@@ -162,7 +253,12 @@ wrangle tyName WrangleOpts {..} = do
         convert =
             FunD (mkName convertName) convClauses
 
-    sig <- [t| $(conT tyName) -> $(conT newRecName)|]
+    let sig = functionType (newTypes ++ [ConT tyName, ConT newRecName])
+        functionType xs = case reverse xs of
+            typ:typs ->
+                foldr (\x acc -> AppT (AppT ArrowT x) acc) typ typs
+            [] ->
+                error "Error in RecordWrangler: needed a nonempty list of types"
 
     pure
         [ DataD [] newRecName tyvars Nothing (map (uncurry RecC) newConstrs) []
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE RecordWildCards #-}
@@ -21,6 +22,24 @@ funWithViewPatterns :: Person -> IO ()
 funWithViewPatterns (wranglePersonToPerson' -> Person'{..}) = do
     putStrLn name'
     print age'
+
+data Dog = Dog { dogFriendly :: Bool }
+
+wrangle ''Dog defWrangleOpts
+    { addFields = [field "dogAge" ''Int]
+    }
+
+convertDog :: Dog -> Int -> Dog'
+convertDog = wrangleDogToDog'
+
+data User = User { userId :: Int }
+
+wrangle ''User defWrangleOpts
+    { addFields = [field "userName" [t|Char -> String|]]
+    }
+
+convertUser :: User -> (Char -> String) -> User'
+convertUser = wrangleUserToUser'
 
 main :: IO ()
 main = do
